8efb5313e7ec050b4231f36bdba4794555175cba
Aaron Fisher <aaronfisher.mt@gmail.com>
2013-12-05 17:50:19 +0000

    Tweaks

--- js/location.js
+++ js/location.js
@@ -14,7 +14,8 @@ function showPosition(position){
 	var catid = document.getElementById("catid").value;
 	var radius = document.getElementById("radius").value;
 
-	string = "client_id=QSULZYWK4P3JMQCJLYTBQHUD1J5Z2YQFHKALPWVABXVVEX0S&client_secret=GGXYSV5OLOFSVD0Q03UOUMXBRRIMQAKZR4BKUAA40ZRIHQQH&ll="+latitude+","+longitude+"&v=20130815&limit=20&categoryId="+catid+"&radius="+radius;
+	//swap out these details below fir your FourSquare app details
+	string = "client_id=YOURCLIENTID&client_secret=YOURCLIENTSECRET="+latitude+","+longitude+"&v=20130815&limit=20&categoryId="+catid+"&radius="+radius;
 	url = "ba-simple-proxy.php?url=https://api.foursquare.com/v2/venues/search?"+encodeURIComponent(string);
 	console.log(url);
 
